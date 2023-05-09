95ef0eb9542b5dbf2ced97110e91dfac4fd92fd1
OliJeffery <o.jeffery@gmail.com>
2023-05-09 02:13:51 +0100

    Cell gets its own component

---
+++ src/components/HeroCell.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hero" :style="`background-image: url(${cell.content.background.filename})`">
+    <div class="hero_info">
+      <img class="logo" :src="cell.content.logo.filename" :alt="cell.content.link.name"/>
+      <router-link :to="cell.content.link.full_slug" class="button">
+        {{cell.content.link_text}}
+      </router-link>
+    </div>
+  </div>
+</template>
+<script setup>
+
+const props = defineProps({
+  "cell": {
+    type: Object,
+    required: true
+  }
+})
+
+</script>
+<style lang="scss" scoped>
+.hero {
+  max-width: 100%;
+  background-size: 100% auto;
+  background-position: center 40%;
+  display: grid;
+  justify-items: center;
+  align-content: center;
+  aspect-ratio: 22/9;
+  transition: background-size .5s ease-in-out;
+  clip-path: polygon(0 0, 100% 1%, 100% 66%, 0% 100%);
+
+  &:hover {
+    background-size: 110% auto;
+  }
+
+  .hero_info {
+    width: 66%;
+    grid-gap: 1rem;
+    display: grid;
+    justify-items: center;
+    align-content: center;
+    margin-top: -7.5%;
+
+    .logo {
+      max-width: 45rem;
+      width: 100%
+    }
+  }
+}
+</style>
--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,18 +1,13 @@
 <template>
   <div v-if="content">
-    <div class="hero" v-for="cell in content.hero" :style="`background-image: url(${cell.content.background.filename})`">
-      <div class="hero_info">
-        <img class="logo" :src="cell.content.logo.filename" :alt="cell.content.link.name" />
-        <router-link :to="cell.content.link.full_slug" class="button">
-          {{cell.content.link_text}}
-        </router-link>
-      </div>
-    </div>
+    <HeroCell v-for="cell in content.hero" :cell="cell" :key="cell.name"/>
   </div>
 </template>
 <script setup>
 import {useStoryblok} from "@storyblok/vue";
-import {ref, onMounted} from "vue";
+import {onMounted, ref} from "vue";
+import HeroCell from "./HeroCell.vue";
+
 const content = ref(null)
 
 
@@ -30,32 +25,3 @@ onMounted(
 
 </script>
 
-<style lang="scss" scoped>
-.hero {
-  max-width: 100%;
-  background-size: 100% auto;
-  background-position: center 40%;
-  display: grid;
-  justify-items: center;
-  align-content: center;
-  aspect-ratio: 22/9;
-  transition: background-size .5s ease-in-out;
-  clip-path: polygon(0 0, 100% 1%, 100% 66%, 0% 100%);
-  &:hover {
-    background-size: 110% auto;
-  }
-  .hero_info {
-    width: 66%;
-    grid-gap: 1rem;
-    display: grid;
-    justify-items: center;
-    align-content: center;
-    margin-top:-7.5%;
-    .logo {
-      max-width: 45rem;
-      width:100%
-    }
-  }
-
-}
-</style>
