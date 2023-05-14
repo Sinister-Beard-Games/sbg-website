a551ac4c910ed4bccf4720a86d15b46ed5185dab
OliJeffery <o.jeffery@gmail.com>
2023-05-14 19:37:09 +0100

    Separated header to own component  in prep to move basket there

---
+++ src/components/shared/SiteHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <router-link to="/">
+      <img :src="logo" alt="Sinister Beard Games" class="site_logo"/>
+    </router-link>
+  </header>
+</template>
+
+<script setup>
+
+const props = defineProps(
+    {
+     logo: {
+      type: String,
+      required: true
+    }
+  }
+)
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
