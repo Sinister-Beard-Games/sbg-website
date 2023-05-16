c27b4595adac98c164494988f7e70c82d4132eef
OliJeffery <o.jeffery@gmail.com>
2023-05-16 18:48:11 +0100

    Basic table formatting for basket and add and remove buttons

--- src/components/basket/ShoppingBasket.vue
+++ src/components/basket/ShoppingBasket.vue
@@ -1,19 +1,31 @@
 <template>
   <SimpleHero :featuredImage="content.content.hero_image.filename" />
-  <table class="basket_items">
-    <tr>
-      <th>Item</th>
-      <th>Number</th>
-      <th>Cost</th>
-      <th>Total</th>
-    </tr>
-    <tr v-for="item in summarisedItems">
-      <td>{{item.item.description}}</td>
-      <td>{{item.count}}</td>
-      <td>£{{itemPrice(item)}}</td>
-      <td>£{{itemPrice(item) * item.count}}</td>
-    </tr>
-  </table>
+  <div class="heading_container">
+    <h1>{{content.name}}</h1>
+  </div>
+  <div class="basket_container">
+    <table class="basket_items">
+      <tr>
+        <th>Item</th>
+        <th>Number</th>
+        <th>Cost</th>
+        <th>Total</th>
+        <th></th>
+      </tr>
+      <tr v-for="item in summarisedItems">
+        <td>{{item.item.description}}</td>
+        <td>{{item.count}}</td>
+        <td>£{{itemPrice(item)}}</td>
+        <td>£{{itemPrice(item) * item.count}}</td>
+        <td>
+          <div class="adjust">
+            <a class="round secondary button" @click="addAnother(item)">+</a>
+            <a class="round secondary button" @click="remove(item)">-</a>
+          </div>
+        </td>
+      </tr>
+    </table>
+  </div>
 
 
 </template>
@@ -31,6 +43,16 @@ const error = ref(null)
 
 const itemPrice = (item) => {
   return item.item.sales_price ? item.item.sales_price : item.item.price
+}
+
+const compare = ( a, b ) => {
+  if ( a.description < b.description ){
+    return -1;
+  }
+  if ( a.description > b.description ){
+    return 1;
+  }
+  return 0;
 }
 
 const basket = useBasketStore()
@@ -51,10 +73,28 @@ const summarisedItems = computed(
           }
         }
     )
-    console.log({items})
-    return items
+    const arrayOfItems = Object.values(items)
+    arrayOfItems.sort(compare)
+    return arrayOfItems
   }
 )
+
+const remove = (basketItem) => {
+  console.log('remove')
+  for (let i=0; i < basket.items.length; i++) {
+    const item = basket.items[i]
+    if(item._uid === basketItem.item._uid) {
+      basket.items.splice(i, 1)
+      return
+    }
+  }
+}
+
+const addAnother = (item) => {
+  basket.items.push(item.item)
+}
+
+
 
 const route = useRoute()
 
@@ -76,5 +116,37 @@ refreshPage()
 </script>
 
 <style lang="scss" scoped>
+.basket_container {
+  padding: 3rem 9rem;
+  table {
+    background: rgba(255,255,255,.3);
+    border-collapse: collapse;
+    border: 1px solid #fff;
+    border-radius: .25rem;
+    color: whitesmoke;
+    tr {
+      border: 1px solid rgba(255,255,255,.2);
+    }
+    td, th {
+      padding: 1rem;
 
+
+    }
+    th {
+      background: rgba(0,0,0,.5);
+      color: whitesmoke;
+      font-weight: 600;
+    }
+  }
+}
+.adjust {
+  display: grid;
+  grid-gap: 1rem;
+  grid-template-columns: 1fr 1fr;
+  .round {
+    height: 1.5rem;
+    width: 1.5rem;
+    cursor: pointer;
+  }
+}
 </style>
